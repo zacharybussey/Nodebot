400198b600fe18be335866d0a0efd2cebbf6271c
Zach <zach.bussey@improvingenterprises.com>
2015-02-01 16:30:24 -0500

    streams.

--- bot.js
+++ bot.js
@@ -1,9 +1,13 @@
 var five = require("johnny-five");
+var rx = require('rx');
+var _ = require('underscore');
+
 var board = new five.Board();
 
 var stdin = process.stdin;
 stdin.setRawMode(true);
 stdin.resume();
+
 
 board.on("ready", function () {
     var wheels = {
@@ -29,13 +33,13 @@ board.on("ready", function () {
             console.log("pivotRight");
         },
         turnLeft: function() {
-        	wheels.left.cw();
-        	wheels.right.stop();
+        	wheels.left.ccw();
+        	wheels.right.cw(.3);
         	console.log("turnLeft");
         },
         turnRight: function () {
-        	wheels.left.stop();
-        	wheels.right.ccw();
+        	wheels.left.ccw(.3);
+        	wheels.right.cw();
         	console.log("turnRight");
         },
         back: function () {
@@ -44,45 +48,56 @@ board.on("ready", function () {
         }
     };
 
+    var keys = [
+		'up',
+		'down',
+		'left',
+		'right',
+		'w',
+		'a',
+		's',
+		'd',
+		'space'
+		];
+
     wheels.stop();
     console.log("Use the cursor keys or ASWD to move your bot. Hit escape or the spacebar to stop.");
 
-    stdin.on("keypress", function(chunk, key) {
-        if (!key) return;
+	var keyPressStream = rx.Observable.fromEvent(stdin, 'keypress',
+		function(args){
+			return args[1];
+	});
 
-        switch (key.name) {
-        case 'up':
-        case 'w':
-            wheels.forward();
-            break;
+	var keyPresses = keyPressStream
+	.filter(function(key){
+		return _.contains(keys, key.name);
+	})
+	.map(function(key){
+		if(!key) return;
+		switch(key.name) {
+			case 'up':
+			case 'w':
+				return wheels.forward;
+				break;
+			case 'down':
+			case 's':
+				return wheels.back;
+				break;
+			case 'left':
+			case 'a':
+				return wheels.turnLeft;
+				break;
+			case 'right':
+			case 'd':
+				return wheels.turnRight;
+				break;
+			case 'space':
+				return wheels.stop;
+				break;
+		}
+	});
 
-        case 'down':
-        case 's':
-            wheels.back();
-            break;
-
-        case 'left':
-        case 'a':
-        	if(key.shift){
-	            wheels.pivotLeft();
-            } else {
-            	wheels.turnLeft();
-            }
-            break;
-
-        case 'right':
-        case 'd':
-        	if(key.shift) {
-	            wheels.pivotRight();
-        	} else {
-        		wheels.turnRight();
-        	}
-            break;
-
-        case 'space':
-        case 'escape':
-            wheels.stop();
-            break;
-        }
-    });
+	keyPresses.subscribe(function(key){
+		key();
+	});
 });
